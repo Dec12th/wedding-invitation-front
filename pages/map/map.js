0c1228e299d8188abd31462b18682e5a7ee21bb3
opg-yin-beibei <yin.beibei@opg.cn>
2018-10-17 15:42:27 +0800

    婚礼请柬-V0.0.1

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,66 +1,54 @@
-// pages/map/map.js
+var api = require('../../api/api.js')
+var app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    markers: [{
+      iconPath: "/image/nav.png",
+      id: 0,
+      latitude: 37.895525,
+      longitude: 117.398193,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  regionchange(e) {
+    console.log(e.type)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  markertap(e) {
+    console.log(e.markerId)
+    wx.openLocation({
+      latitude: 29.317570,
+      longitude: 111.676420,
+      scale: 18,
+      name: '王家湾',
+      address: '湖南省常德市临澧县四新岗镇毛花村王家湾组'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  controltap(e) {
+    console.log(e.controlId)
   }
 })
